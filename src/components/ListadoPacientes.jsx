086976b308072fd2ffd3b91f76784c5dc4c0cdbe
Luis Fernando Venegas Avila <53911844+Crowexe@users.noreply.github.com>
2023-04-25 23:14:17 -0600

    props modified

--- src/components/ListadoPacientes.jsx
+++ src/components/ListadoPacientes.jsx
@@ -5,23 +5,20 @@ const ListadoPacientes = ({pacientes}) => {
   return (
     <>
         <div className="md:w-1/2 lg:w-3/5 md:h-screen overflow-y-scroll">
-        	<h2 className="font-black text-3xl text-center">Listado de Pacientes</h2>
-			<p className="text-lg mt-5 text-center mb-10 font-bold">
-			Administra tus {''}
-				<span className="text-indigo-600">Pacientes y Citas</span>
-			</p>	
+          <h2 className="font-black text-3xl text-center">Listado de Pacientes</h2>
+      <p className="text-lg mt-5 text-center mb-10 font-bold">
+      Administra tus {''}
+        <span className="text-indigo-600">Pacientes y Citas</span>
+      </p>	
 
-	    {pacientes.length !== 0 ? 
-            pacientes.map( paciente => (
-                <Paciente                                                         
-                    paciente={paciente}                                                   
-                />                    
-                )) : (
-                <p>No hay pacientes aún</p>
-            )
-        }
+      {pacientes.map( paciente => (
+          <Paciente                                                         
+              paciente={paciente}                                                   
+          />                    
+          )) 
+      }
 
-			<Paciente />
+      <Paciente />
 
         </div>
     </>
